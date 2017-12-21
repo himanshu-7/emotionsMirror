79f36e4aee4e5e85a39eb62292efaca6c69b5dbd
Chinmay19 <chinmay.pathak14@gmail.com>
2017-12-21 18:12:57 +0100

    TP-link

---
+++ light_bulb.py
@@ -0,0 +1,61 @@
+import requests
+
+headers = {
+    'Content-Type': 'application/json',
+}
+
+#hello world
+data_1 = '{"method": "helloCloud","params": { "appPackageName": "com.tplink.kasa_android", "appType": "Kasa_Android", "tcspVer" : 1.1, "terminalUUID": "e36193be-f046-43d4-9669-ceab734b05a3"}}'
+response = requests.post('https://wap.tplinkcloud.com/', headers=headers, data=data_1)
+
+
+#get token
+data_2 = '{"method": "login","params": { "appType": "Kasa_Android", "cloudPassword" : "15111992k","cloudUserName": "himanshu.7.shah@gmail.com", "terminalUUID": "e36193be-f046-43d4-9669-ceab734b05a3"}}'
+response = requests.post('https://wap.tplinkcloud.com/', headers=headers, data=data_2)
+r = response.json()
+
+print ("%s" %r)
+token1 = r['result']['token']
+print ("%s" %token1)
+
+
+
+
+#get device list
+data_3 = '{"method": "getDeviceList","params": {}}'
+response = requests.post('https://wap.tplinkcloud.com/?token=%s'%token1,headers=headers, data=data_3)
+r = response.json()
+print ("%s" %r)
+device1 = r['result']['deviceList'][0]['deviceId']
+print ("%s" %device1)
+
+#on
+# x = {
+# smartlife.iot.smartbulb.lightingservice: [
+# transition_light_state: [
+# {on_off: '1'}
+# {transition_period: '50'}]
+# ]
+# 	}
+
+
+
+
+data_4 ='{"method": "passthrough", "params":{"deviceId": "8012C3C1DED0BCD794179E8675A70FE9189105B5", "requestData": "{\"smartlife.iot.smartbulb.lightingservice\":{\"transition_light_state\": {\"on_off\": 1}}}"}}'
+
+
+response = requests.post('https://wap.tplinkcloud.com/?token=%s'%token1,headers=headers, data=data_4)
+# r = response.json()
+# print ("%s" %r)
+
+
+'''
+params = (
+    ('token', '74adcc7e-64f7-47c1-a751-dece6d2f4704 HTTP/1.1'),
+)
+
+data = '{"method":"passthrough", "params": {"deviceId": device1, "requestData": "{\\"system\\":{\\"set_relay_state\\":{\\"state\\":1}}}" }}'
+
+response = requests.post('https://eu-wap.tplinkcloud.com/', headers=headers, params=params, data=data)
+
+'''
